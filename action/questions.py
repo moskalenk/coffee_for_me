492b5000800880ff533a5efa6e75139456bf78b9
konstantin <folky93@inbox.ru>
2019-05-15 23:48:58 +0300

    change questions

--- action/questions.py
+++ action/questions.py
@@ -3,6 +3,17 @@
 * Checkbox question example
 * run example by typing `python example/checkbox.py` in your console
 """
+from PyInquirer import Validator, ValidationError
+
+
+class NumberValidator(Validator):
+    def validate(self, document):
+        try:
+            int(document.text)
+        except ValueError:
+            raise ValidationError(
+                message='Please enter a number',
+                cursor_position=len(document.text))  # Move cursor to end
 
 
 def coffee_questions(coffee_list_from_db, additional_ingridients_from_db):
@@ -10,12 +21,19 @@ def coffee_questions(coffee_list_from_db, additional_ingridients_from_db):
     additional_ingridients_list = list(map(lambda el: {"name": el}, additional_ingridients_from_db))
     my_coffee_questions = [
         {
-            'type': 'checkbox',
-            'message': 'Select coffee',
-            'name': 'coffee',
+            'type': 'list',
+            'message': 'What coffee do you need',
+            'name': 'coffee type',
             'choices': coffee_wit_price_list,
             'validate': lambda answer: 'You must choose at least one topping.' if len(answer) == 0 else True
             # check empty answer
+        },
+        {
+            'type': 'input',
+            'name': 'quantity',
+            'message': 'Count of servings do you need?',
+            'validate': NumberValidator,
+            'filter': lambda val: int(val)
         },
         {
             'type': 'checkbox',
