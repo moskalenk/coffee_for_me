1d6c7468e247ed6f5677f08c00558114c5cbe426
konstantin <folky93@inbox.ru>
2019-05-25 00:06:31 +0300

    add tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,10 @@
 import pytest
 
 from db.cafe_db import CafeDB
+from db.connection import DBConnection
 from roles import Salesman, Manager
 from services.processing_service import ProcessingService
+from services.reporting_service import ReportingService
 
 
 @pytest.fixture()
@@ -18,10 +20,12 @@ def create_manager_object():
 @pytest.fixture()
 def mocked__cafe_db__select_as_list(mocker):
     list_of_names = ["lisa", "Bob"]
+
+    mocker.patch.object(DBConnection, "__init__", lambda db_name: None)
     mocker.patch.object(CafeDB, "select_as_list")
     CafeDB.select_as_list.return_value = list_of_names
-    cafe_db = CafeDB("test_db_coffee.db")
-    return cafe_db, list_of_names
+    cafe_db_obj = CafeDB()
+    return cafe_db_obj, list_of_names
 
 @pytest.fixture()
 def belonging_name_to_role(mocker):
@@ -34,3 +38,12 @@ def belonging_name_to_role(mocker):
     processing_service = ProcessingService(cafe_db)
 
     return processing_service
+
+
+@pytest.fixture()
+def show_common_report_mocker(mocker):
+    create_summary_table_mocker = mocker.patch.object(ReportingService, "_create_summary_table")
+    text_for_print = create_summary_table_mocker.return_value = "common report here"
+    mocker.patch.object(ReportingService, "__init__", lambda cafe_db: None)
+    reporting_service_obj = ReportingService()
+    return reporting_service_obj, text_for_print
